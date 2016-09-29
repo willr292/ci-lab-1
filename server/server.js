cad323c8d5b8561dd5b315079165d76224568f6b
Simon Hall <simonhall-scottlogic@users.noreply.github.com>
2016-09-29 15:09:44 +0100

    Made the static directory reference relative to the server directory, not the working directory as it used to be.  Allows us to run the node command from any directory we like.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('../site'));
+app.use(express.static(__dirname+'/../site'));
 
 app.listen(3000, function() {
   console.log('Serving static site on port 3000');
