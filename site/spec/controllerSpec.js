962cddcd00055eb46b3c02fa782dfa6374a510e0
Simon Hall <simonhall-scottlogic@users.noreply.github.com>
2016-10-03 14:15:13 +0100

    First test case works now

--- site/spec/controllerSpec.js
+++ site/spec/controllerSpec.js
@@ -13,41 +13,27 @@ function createModel() {
 	      ],
 
 				crossAt: function(row, col) {
-					console.log('crossAt called')
-
 					// TODO have I used 'this' correctly?
 					this.board[row][col] = "cross";
+					return this;
+				},
+				noughtAt: function(row, col) {
+					// TODO have I used 'this' correctly?
+					this.board[row][col] = "nought";
 					return this;
 				}
 	    };
 }
 
 describe("updateModel", function() {
-	// TODO google for common js module format
 	var controller = require('../controller.js');
-  var beforeModel;
-	// var beforeModel = createModel();//.crossAt(0, 0);
-
-  beforeEach(function() {
-    beforeModel = createModel().crossAt(0, 0);
-		// beforeModel = createModel();
-  });
-
-	it("initialises the board in beforeAll", function() {
-		expect(beforeModel).toBeTruthy();
-		expect(beforeModel.board[0][0]).toEqual("cross");
-	});
-
-  // afterAll(function() {
-  //   foo = 0;
-  // });
 
   it("does nothing if the cell is already taken", function() {
-
-		var afterModel = controller.updateModel(beforeModel, 0, 0);
-
+		var beforeModel = createModel().noughtAt(0, 0);
+		beforeModel.isNoughtToPlay = false;
+		var afterModel = controller.applyClick(beforeModel, 0, 0);
     expect(afterModel.isGameOver).toEqual(false);
-    // foo += 1;
+		expect(afterModel.isNoughtToPlay).toEqual(false);
   });
 
 });
